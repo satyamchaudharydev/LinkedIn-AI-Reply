fbf3db6cc8d8e0ed1652137b7ef4358fa6935590
satyam_chaudhary <satyam.chaudhary@brightchamps.com>
2024-10-09 20:56:43 +0530

    initial changes

--- components/Interface.tsx
+++ components/Interface.tsx
@@ -43,17 +43,6 @@ export function AIAssistant({ onInsert }: AIAssistantProps) {
       onInsert(lastAiMessage.content);
     }
   };
-{/* <section class="">
-  <form class="flex gap-1 justify-center w-full font-medium text-gray-400 rounded-xl shadow-sm min-h-[61px]">
-    <label for="promptInput" class="sr-only">Your prompt</label>
-    <input
-      type="text"
-      id="promptInput"
-      placeholder="Your prompt"
-      aria-label="Your prompt"
-    />
-  </form> */}
-//   <button class="flex overflow-hidden gap-2.5 justify-center items-center px-6 py-3 mt-7 font-semibold text-white whitespace-nowrap bg-blue-500 rounded-lg max-md:px-5"></button>
   return (
     <div className="space-2">
       <div className="max-h-64 overflow-y-auto space-y-2 pr-2">
@@ -104,7 +93,6 @@ export function AIAssistant({ onInsert }: AIAssistantProps) {
                          active:!shadow-none"
         />
         <button onClick={handleGenerate}>
-          {/* <Send size={20} /> */}
           <span className="sr-only">Send message</span>
         </button>
       </div>
@@ -114,8 +102,6 @@ export function AIAssistant({ onInsert }: AIAssistantProps) {
             <button
               type="button"
               onClick={handleInsert}
-
-              // onClick={handleInsertResponse}
               className="text-[#666D80] border-2 border-solid border-current flex overflow-hidden gap-2.5 justify-center items-center px-6 py-3 mt-7 font-semibold whitespace-nowrap  rounded-[8px] max-md:px-5"
             >
               {" "}
@@ -144,16 +130,10 @@ export function AIAssistant({ onInsert }: AIAssistantProps) {
         ) : (
           <button
             onClick={handleGenerate}
-            
             type="button"
             className="flex overflow-hidden gap-2.5 justify-center items-center px-6 py-3 mt-7 font-semibold text-white whitespace-nowrap bg-blue-500 rounded-lg max-md:px-5"
           >
-            <svg
-              width="18"
-              height="18"
-              viewBox="0 0 25 25"
-              fill="none"
-            >
+            <svg width="18" height="18" viewBox="0 0 25 25" fill="none">
               <path
                 d="M24.456 11.6075L2.45599 0.607504C2.28356 0.521271 2.08988 0.486719 1.89827 0.508009C1.70665 0.529299 1.52528 0.605523 1.37599 0.727504C1.23341 0.846997 1.12699 1.00389 1.0687 1.18055C1.0104 1.35721 1.00254 1.54662 1.04599 1.7275L4.00599 12.4975L1.00599 23.2375C0.965214 23.3886 0.960455 23.5471 0.992092 23.7003C1.02373 23.8535 1.09088 23.9972 1.18815 24.1198C1.28541 24.2423 1.41008 24.3403 1.55212 24.4059C1.69416 24.4715 1.84962 24.5029 2.00599 24.4975C2.16253 24.4966 2.31667 24.4589 2.45599 24.3875L24.456 13.3875C24.6198 13.3036 24.7573 13.1761 24.8532 13.0191C24.9492 12.862 25 12.6816 25 12.4975C25 12.3135 24.9492 12.133 24.8532 11.9759C24.7573 11.8189 24.6198 11.6914 24.456 11.6075ZM3.55599 21.6075L5.76599 13.4975H15.006V11.4975H5.76599L3.55599 3.3875L21.766 12.4975L3.55599 21.6075Z"
                 fill="white"
